a945c6b96aa646a163aae2c2a26ae2771f7fbc94
pradeepselvakumar <pradeep@selvakumar.net>
2024-10-12 14:58:12 -0400

    Removed Book

--- Book/js/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const tocBtn = document.querySelector(".toc-btn");
-    const tocList = document.querySelector(".toc-list");
-
-    tocBtn.addEventListener("click", function() {
-        tocList.classList.toggle("show");
-    });
-});
